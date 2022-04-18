d6f246c9eb7f88ba2a797a61ab7e5f66e2f942c8
VLSMB <36068250+VLSMB@users.noreply.github.com>
2022-04-18 09:30:41 +0800

    Update calc.asm

--- calc.asm
+++ calc.asm
@@ -6,7 +6,10 @@ data segment
 choice	db 0AH,0DH,"Please choose:$"
 illeagal	db 0AH,0DH,"illeagal input!$"
 debug	db 0AH,0DH,"Haven't finished!$",0AH,0DH
-input_num db '#',00H,00H,00H,00H,00H,'#' ;暂时只提供16位加减法，32位以后再补吧…
+first  db 0AH,0DH,"Input the first number(only 10 numbers):$"
+second  db 0AH,0DH,"Input the second number(only 10 numbers):$"
+input_num_one db '#',00H,00H,00H,00H,00H,00H,00H,00H,00H,00H,'#',00H;暂时只提供32位加减法，64位以后再补吧…
+input_num_two db '#',00H,00H,00H,00H,00H,00H,00H,00H,00H,00H,'#',00H
 data ends
 stack segment
 	db 128 dup(0)
@@ -61,11 +64,25 @@ div_before:
 do_add:
 	push ax
 	push dx
+	push bx
+	
+	mov dx,offset first
+	mov ah,09H
+	int 21h
+	mov bx,offset input_num_one
+	call get_input   ; bx为数字存放地址
+	call debug_output
+	mov dx,offset second
+	mov ah,09H
+	int 21h
+	mov bx,offset input_num_two
 	call get_input
 	call debug_output
+	
 	mov dx,offset debug
 	mov ah,09H
 	int 21h
+	pop bx
 	pop dx
 	pop ax
 	ret
@@ -89,18 +106,30 @@ do_div:
 	ret
 
 get_input:
-	; offset input_num 为数据段中存放数字的位置，ds固定为数据段，数据为倒序，即12345->54321
+	; ds固定为数据段，数据为倒序，即12345->54321
+	; bx为offset input_num 即数据段中存放数字的位置
 	push ax
 	push bx
 	push si
-	mov bx,offset input_num
-	add bx,5
-	mov si,5
+	push cx
+	add bx,10
+	mov si,10
+	; 初始化内存空间
+	mov cx,10
+	push cx
+	push bx
+i_i:	mov byte ptr [bx],0
+	dec bx
+	loop i_i
+	pop bx
+	pop cx
 reget:
 	cmp si,0
 	je input_got
 	mov ah,0
 	int 16h
+	cmp ah,1CH
+	je get_enter
 	cmp ah,02H
 	jb reget
 	cmp ah,0BH
@@ -112,32 +141,43 @@ reget:
 	dec si
 	jmp reget
 input_got:
+	pop cx
 	pop si
 	pop bx
 	pop ax
 	ret
+get_enter:
+	; 中途按Enter后的处理
+	cmp si,10
+	je input_got
+	mov cx,si
+g_e:	
+	mov byte ptr [bx],23H
+	dec bx
+	loop g_e
+	jmp input_got
 
 debug_output:
 	; 测试看字符串是否输入进去
+	; bx为offset input_num 即数据段中存放数字的位置
 	push bx
 	push cx
 	push ax
 	push si
 	push dx
-	mov bx,offset input_num
 	inc bx
-	mov cx,5
+	mov cx,10
 	mov si,0
 s:	add byte ptr [bx+si],30H
 	inc si
 	loop s
 	mov byte ptr [bx+si+1],24H
-	mov dx,offset input_num
+	mov dx,offset input_num_one
 	mov ah,09H
 	int 21h
 	
 	mov si,0
-	mov cx,5
+	mov cx,10
 t:	sub byte ptr [bx+si],30H
 	inc si
 	loop t
